8894daf8c74d1a9a59e88cdd1b5a7b53974a56c9
Muntaha <muntahamemon2001@gmail.com>
2026-05-20 20:45:01 +0500

    Use Vercel static + API routing

--- api/index.py
+++ api/index.py
@@ -1,16 +1,5 @@
 import sys
 import os
-
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
-
-from fastapi.staticfiles import StaticFiles
 from backend.main import app
-
-frontend_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "frontend")
-
-try:
-    app.mount("/static", StaticFiles(directory=frontend_path), name="static")
-except:
-    pass
-
 handler = app
